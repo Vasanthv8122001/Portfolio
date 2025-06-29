b71bf478d8b1f43c2257b0f157f873f2ced6746b
Vasanth V <vasanthv8122001@gmail.com>
2025-06-29 11:48:33 +0530

    Added

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const router = require('./routes/index');
+// const connectDB = require('./config/db');
+dotenv.config();
+
+// connectDB();
+app.use(cors({
+  origin: '*', // Allow all origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
+app.use(express.text());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api', router)
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'API is running' });
+});
+
+app.listen(3001, () => {
+  console.log('API server is running on port 3001');
+});
