9ba9552fb7985c69693406ac6a3751206387b6d3
Vasanth V <vasanthv8122001@gmail.com>
2025-07-06 13:20:41 +0530

    Api file deleted

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { notification } = require('../controller/notification');
-const { sendEmail } = require('../common/mail');
-
-router.post('/notification', notification)
-router.post('/sendEmail', sendEmail)
-
-module.exports = router;
